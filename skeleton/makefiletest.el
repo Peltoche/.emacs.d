a809a1125ed0571ef7fa693e007787ab841ee9af
ppeltier <ppeltier@student.42.fr>
2014-11-11 02:27:58 +0100

    simplification du skeleton de Makefile

--- skeleton/makefiletest.el
+++ skeleton/makefiletest.el
@@ -6,31 +6,41 @@
 ;    By: ppeltier <ppeltier@student.42.fr>          +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
 ;    Created: 2014/11/09 14:52:26 by ppeltier          #+#    #+#              ;
-;    Updated: 2014/11/11 02:25:42 by ppeltier         ###   ########.fr        ;
+;    Updated: 2014/11/11 02:03:53 by ppeltier         ###   ########.fr        ;
 ;                                                                              ;
 ;******************************************************************************;
 
 ;; Makefiles's Skeleton
-(define-skeleton create-makefile
+(define-skeleton create-makefiletest
   "Create a Makefile."
   ""
   "\n"
   "NAME		= # Put your binary name"
   "\n"
+  (progn
+	(setq src_fold (skeleton-read "Sources Folder: "))
+	(if (equal src_fold ".")
+		""
+	  "OBJDIR		= "))
+	(if (equal src_fold ".")
+		""
+	  src_fold)
   "\n"
   "\n"
-  "SRC			= # Put your sources"
+  "SRC			= "
   "\n"
+  "LIB			= -Llibft/ -lft\n"
+  "INC			= -I -Iinclude/\n"
   "\n"
   "OBJ			= $(SRC:.c=.o)\n"
   "CC			= /usr/bin/gcc\n"
   "CFLAGS		= -Wall -Wextra -Werror\n"
-  "CDEBUG		= -W -Wall -ansi -pedantic -g\n"
+  "CDEBUG		= -W -Wall -ansi -pedantic -g -ggdb\n"
   "RM			= /bin/rm -f\n"
   "ECHO			= /bin/echo -e\n"
   "\n"
   "$(NAME)	:		$(OBJ)\n"
-  "				@$(CC) $(INC) $(CFLAGS) -o $(NAME) $(OBJ)\n"
+  "				$(CC) $(INC) $(CFLAGS) -o $(NAME) $(OBJ) $(LIB)\n"
   "				@$(ECHO) '\\033[0;32m> Compiled\\033[0m'\n"
   "\n"
   "clean	:\n"
@@ -49,8 +59,8 @@
   ".PHONY	:		all clean re\n"
   "\n"
   "debug	:		$(OBJ)\n"
-  "				@$(CC) $(INC) $(CDEBUG) -o $(NAME) $(OBJ)\n"
+  "				@$(CC) $(INC) $(CDEBUG) -o $(NAME) $(OBJ) $(LIB)\n"
   "				@$(ECHO) '\\033[0;32m> Mode Debug: done\\033[0m'\n"
   "\n"
   ".c.o	:\n"
-  "				$(CC) $(CFLAGS) -o $@ -c $<\n")
+  "				$(CC) $(INC) $(CFLAGS) -o $(OBJDIR)/$@ -c $<\n")
